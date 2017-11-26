7f62711a01c2f367cd717418165c384983166d5e
gnu3ra <gnu3ra@riseup.net>
2017-11-26 02:25:36 -0500

    foreign keys

--- iitthing/loginpage/models.py
+++ iitthing/loginpage/models.py
@@ -3,27 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-
-class User(models.Model):
-    name = models.CharField(unique=True, max_length=255)
-    password = models.CharField(max_length=255)
-    online = models.BooleanField()
-    sessionid = models.IntegerField(unique=True)
-    usertype = models.CharField(max_length=255)
-
-
-class Session(models.Model):
-    sessionid = models.IntegerField(unique=True)
-    topic = models.CharField(max_length=255)
-    roomid = models.IntegerField(unique=True)
-    courseid = models.IntegerField(unique=True)
-    
-
 class Room(models.Model):
     roomid = models.IntegerField(unique=True)
     roomnumber = models.IntegerField()
     building = models.CharField(max_length=255)
-
 
 class Course(models.Model):
     courseid = models.IntegerField(unique=True)
@@ -31,4 +14,18 @@ class Course(models.Model):
     coursesubject = models.CharField(max_length=255)
     coursenum = models.IntegerField()
     sectionnum = models.IntegerField()
-    
+ 
+class Session(models.Model):
+    sessionid = models.IntegerField(unique=True)
+    topic = models.CharField(max_length=255)
+    roomid = models.ForeignKey(Room, on_delete=models.CASCADE)
+    courseid = models.ForeignKey(Course, on_delete=models.CASCADE)
+ 
+class User(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+    password = models.CharField(max_length=255)
+    online = models.BooleanField()
+    sessionid = models.ForeignKey(Session, on_delete=models.CASCADE)
+    usertype = models.CharField(max_length=255)
+
+  
