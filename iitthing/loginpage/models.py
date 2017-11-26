64c99a7d80aba842f3569866035f132b6a198620
gnu3ra <gnu3ra@riseup.net>
2017-11-26 02:21:12 -0500

    more models

--- iitthing/loginpage/models.py
+++ iitthing/loginpage/models.py
@@ -6,6 +6,29 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(unique=True, max_length=255)
+    password = models.CharField(max_length=255)
+    online = models.BooleanField()
+    sessionid = models.IntegerField(unique=True)
+    usertype = models.CharField(max_length=255)
 
 
+class Session(models.Model):
+    sessionid = models.IntegerField(unique=True)
+    topic = models.CharField(max_length=255)
+    roomid = models.IntegerField(unique=True)
+    courseid = models.IntegerField(unique=True)
+    
+
+class Room(models.Model):
+    roomid = models.IntegerField(unique=True)
+    roomnumber = models.IntegerField()
+    building = models.CharField(max_length=255)
+
+
+class Course(models.Model):
+    courseid = models.IntegerField(unique=True)
+    coursename = models.CharField(max_length=255)
+    coursesubject = models.CharField(max_length=255)
+    coursenum = models.IntegerField()
+    sectionnum = models.IntegerField()
     
