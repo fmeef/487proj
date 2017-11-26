584a2547ba36a870a65dd648181888162d9024c0
rptjf <bdf>
2017-11-25 19:52:26 -0600

    checkpoint

--- iitthing/loginpage/models.py
+++ iitthing/loginpage/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+    
