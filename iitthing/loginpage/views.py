a61ffa1405b043792b3127d72e6bab57356161e5
gnu3ra <gnu3ra@riseup.net>
2017-11-28 17:40:14 -0600

    redirect to login

--- iitthing/loginpage/views.py
+++ iitthing/loginpage/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from django.shortcuts import redirect
+from django.contrib.auth import models as auth_models 
 
 # Create your views here.
 
 def login(request):
-    template = loader.get_template('login.html')
-    
-    return HttpResponse(template.render( template))
+    return redirect('/login')
 
 def homescreen(request):
     return HttpResponse("Home screen")
@@ -20,6 +19,7 @@ def createnewsession(request):
     return HttpResponse("Create new session screen")
 
 def viewsessionpost(request):
+
     return HttpResponse("View session post screen")
 
 def mapscreen(request):
