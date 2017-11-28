0913165ef79081c910001912607bf40ca842ae3d
gnu3ra <gnu3ra@riseup.net>
2017-11-28 17:59:06 -0600

    cookie based sessions

--- iitthing/loginpage/views.py
+++ iitthing/loginpage/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import redirect
 from django.contrib.auth import models as auth_models 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -18,9 +19,12 @@ def studypostcatalog(request):
 def createnewsession(request):
     return HttpResponse("Create new session screen")
 
-def viewsessionpost(request):
 
-    return HttpResponse("View session post screen")
+def viewsessionpost(request):
+    if request.user.is_authenticated():
+        return HttpResponse("Authorized")
+    else:
+        return HttpResponse("UNAUTHORIZED ACCESS. DISPATCHING GOONS.")
 
 def mapscreen(request):
     return HttpResponse("View session post screen")
