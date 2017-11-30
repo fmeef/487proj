da73ba20885f96eb49486da2225f99cc2df25647
gnu3ra <gnu3ra@riseup.net>
2017-11-29 22:37:37 -0600

    fix attempt 2

--- iitthing/loginpage/views.py
+++ iitthing/loginpage/views.py
@@ -38,7 +38,7 @@ def login(request):
 
 def homescreen(request):
     if request.user.is_authenticated():
-        return render_to_response('index.html', RequestContext(request))
+        return render_to_response('index.html',context_instance=RequestContext(request))
     else:
         return HttpResponse(unauthorizedmsg)
 
@@ -78,13 +78,13 @@ def viewsessionpost(request):
                 print "query ", subject, " failed. Not found" 
                 newcontext["course_list"] = ["not found"]
 
-        return render_to_response('sessions.html', newcontext)
+        return render_to_response('sessions.html', context_instance=newcontext)
     else:
         return HttpResponse(unauthorizedmsg)
 
 def mapscreen(request):
     if request.user.is_authenticated():
-        return render_to_response('map.html', RequestContext(request))
+        return render_to_response('map.html',context_instance= RequestContext(request))
     else:
         return HttpResponse(unauthorizedmsg)
 
